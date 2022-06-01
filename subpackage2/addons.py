f68ba6ed61cbd6de90d420444b1e4328e10f6fa9
Taras Lutsyshyn <taraslut78@gmail.com>
2022-06-01 20:13:50 +0300

    Add logic to mu_foo

--- subpackage2/addons.py
+++ subpackage2/addons.py
@@ -1,10 +1,11 @@
-
 # from ..helper import is_board_completed
 from helper import is_board_completed
 
 
 def my_foo():
-    pass
+    x = 3
+    y = 2
+    return x + y
 
 
 if __name__ == '__main__':
